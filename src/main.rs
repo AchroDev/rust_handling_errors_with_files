5f0908b2e41fd36278e9c093392a27c67b5d38e6
AchroDev <ztblaine@protonmail.com>
2024-05-05 18:11:31 -0500

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::fs::File;
+use std::io::*;
+use std::process::Output;
+
+//Handling errors
+
+fn main() {
+    // Creating file called 'lines.txt'
+    let path = "lines.txt";
+    // Defining the output where the file is created
+    let output = File::create(path);
+    // Handling errors with creating the file
+    let mut output = match output {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("Problem creating file : {:?}", error);
+        }
+    };
+    // Write to the file and define the panic error message to be expected with problems.
+    write!(output, "Just some\nRandom words").expect("Failed to write to file");
+
+    // Open the file and if everything is ok, unwrap and return/read the file
+    let input = File::open(path).unwrap();
+    let buffered = BufReader::new(input);
+    for line in buffered.lines() {
+        println!("{}", line.unwrap());
+    }
+
+    // Handling specific errors
+    let output2 = File::create("rand.txt");
+    let output2 = match output2 {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("rand.txt") {
+                Ok(fc) => fc,
+                Err(error) => panic!("Can't create file : {:?}", error),
+            },
+            _other_error => panic!("Problem opening file : {:?}", error),
+        },
+    };
+}
